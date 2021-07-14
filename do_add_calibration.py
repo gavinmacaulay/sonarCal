129d1eddee6e29adc3dd4fc0ce06d959b8cc11a9
Gavin Macaulay <gavin@macaulay.co.nz>
2021-07-14 19:30:11 +1200

    Tidy up

--- do_add_calibration.py
+++ do_add_calibration.py
@@ -8,12 +8,11 @@ from netCDF4 import Dataset
 # The directory that contains the .nc files 
 dataDir = Path(r'C:\Users\gavin\Data - not synced\temp')
 
-# The beam groups to work on. There can be as many or as few beam groups in 
-# this list:
+# The beam groups to work on. There can be as many or as few beam groups as desired
 beamGroups = ['Beam_group1', 'Beam_group2']
 
 # The new deltaGs to write into the file (should be the same number of elements
-# as in beamGroup - they are used in the order as the beam groups in beamGroups)
+# as in beamGroups, in the same order as in beamGroups)
 deltaGs = [0.0, 0.0] # [dB]
 
 # Use all files in the directory
@@ -25,12 +24,10 @@ for f in files:
     # open file
     root = Dataset(f, "a", format="NETCDF4")
 
-    # Update gain_correction in each beam group.    
+    # Update gain_correction in appropriate variables    
     for deltaG, beamGroup in zip(deltaGs, beamGroups):
-        # Get the group that contains the beam data
-        group = root['Sonar'][beamGroup]
         # Get the variable that holds deltaG
-        g = group['gain_correction']
+        g = root['Sonar'][beamGroup]['gain_correction']
         # Update the values in the variable
         g[:] = deltaG # same value for all beams and all pings
         
